fafd7f19297f123d9a3b5af096ea4ca9b9fa643a
milansoap <milan.seid123@gmail.com>
2023-09-12 14:44:21 +0200

    adding 7-1 sass architecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className="navbar">
+  <div className="navbar-section">
+    <a className="navbar-link">
+      <i className="nav-icon">Icon 1</i>
+    </a>
+    <a className="navbar-link">
+      <i className="nav-icon">Icon 2</i>
+    </a>
+  </div>
+</div>
+
+<button className='btn'> aaaaa </button>
+
+    </>
   );
 }
 
