1a13c193d72af17d71227baa3dd0dc886c1c5acd
milansoap <milan.seid123@gmail.com>
2023-09-12 14:49:05 +0200

    defining models in react

---
+++ src/models/models.ts
@@ -0,0 +1,7 @@
+import { Element } from "./Element";
+import { Configuration } from "./Configuration";
+import { Page } from "./Page";  
+import { User } from "./User";
+
+
+export {Element, Configuration, Page, User}
